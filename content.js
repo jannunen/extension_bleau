263f17fce81697eeb51f3941d17d8b3e171c0b05
Jarmo Annunen <jannunen@koodiorja.com>
2019-08-28 14:31:05 +0300

    Added checkbox addition also to a single problem page.

--- content.js
+++ content.js
@@ -14,15 +14,15 @@ $(document).ready(function() {
       chrome.storage.sync.set(obj, function(foo) {
         //console.log('Value is set to ' + saved + " for key "+key);
         console.log(JSON.stringify(foo));
-           self.parents(".vsr").addClass("done");
-        });
+        self.parents(".vsr").addClass("done");
+      });
     } else {
       console.log("uncheck");
-      
+
       chrome.storage.sync.remove([key], function(foo) {
         self.parents(".vsr").removeClass("done");
       });
-       
+
     }
   });
 
@@ -33,39 +33,46 @@ $(document).ready(function() {
   var m = url.match(/(\d+).html/);
   if (m.length > 1) {
     var pid = m[1];
+    addCheckbox(pid,$("h3"));
     // pid found, check if it's ticked..
-       chrome.storage.sync.get(["p"+pid], function(result) {
-         if (result["p"+pid]!=null) {
-           $("h3").addClass("done");
-            var d = new Date(result["p"+pid]);
-           var datestring = ("0" + d.getDate()).slice(-2) + "." + ("0"+(d.getMonth()+1)).slice(-2) + "." +
-    d.getFullYear() + " " + ("0" + d.getHours()).slice(-2) + ":" + ("0" + d.getMinutes()).slice(-2);
-           $("h3").parent().append("<h4 style='color : red;'>Ticked at " +datestring+"</h4>");
-         }
-       });
+    chrome.storage.sync.get(["p"+pid], function(result) {
+      if (result["p"+pid]!=null) {
+        $("h3").addClass("done");
+        var d = new Date(result["p"+pid]);
+        var datestring = ("0" + d.getDate()).slice(-2) + "." + ("0"+(d.getMonth()+1)).slice(-2) + "." +
+          d.getFullYear() + " " + ("0" + d.getHours()).slice(-2) + ":" + ("0" + d.getMinutes()).slice(-2);
+        $("h3").parent().append("<h4 style='color : red;'>Ticked at " +datestring+"</h4>");
+      }
+    });
   }
 
 });
 
-// Go through the sector listing routes, add the checkboxes and add the done-class if the problem is ticked
-$(".vsr").find("a").each(function() {
-   var t= $(this);
-  var m = t.attr("href").match(/(\d+).html/);
-  if (m.length > 1) {
-    var pid = m[1];
+/**
+ * Adds checkbox in front of given element t
+ */
+var addCheckbox = function(pid,t) {
     var checkbox = $('<input data-pid='+pid+' type="checkbox" name="ticked['+pid+']" class="ticked" value="1" />');
     // Find info from storage if this is already checked..
     if (pid != undefined) {
-       chrome.storage.sync.get(["p"+pid], function(result) {
-         
-         if (result["p"+pid]!=null) {
-         console.log('Value currently is ' + result["p"+pid]);
-           checkbox.attr("checked","checked");
-           t.parents(".vsr").addClass("done");
-         }
-       });
+      chrome.storage.sync.get(["p"+pid], function(result) {
+        if (result["p"+pid]!=null) {
+          console.log('Value currently is ' + result["p"+pid]);
+          checkbox.attr("checked","checked");
+          t.parents(".vsr").addClass("done");
+        }
+      });
     }
     t.prepend(checkbox);
+
+}
+// Go through the sector listing routes, add the checkboxes and add the done-class if the problem is ticked
+$(".vsr").find("a").each(function() {
+  var t= $(this);
+  var m = t.attr("href").match(/(\d+).html/);
+  if (m.length > 1) {
+    var pid = m[1];
+    addCheckbox(pid,t);
   }
 });
 
